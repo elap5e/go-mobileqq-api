5b754d10f1e5ad63e51c5d27f5a174bdb65b86fa
Elapse <75983455+elap5e@users.noreply.github.com>
2021-06-27 17:36:12 +0800

    bug fix: check valid oicq

--- encoding/oicq/decode.go
+++ encoding/oicq/decode.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/hex"
 	"fmt"
+	"io"
 	"log"
 
 	"github.com/elap5e/go-mobileqq-api/bytes"
@@ -12,17 +13,20 @@ import (
 )
 
 func Unmarshal(ctx context.Context, data []byte, msg *Message) error {
-	buf := bytes.NewBuffer(data)
+	n, err := checkVaild(data)
+	if err != nil {
+		return err
+	}
+	buf := bytes.NewBuffer(data[5 : n-1])
 	if err := unmarshalHead(ctx, buf, msg); err != nil {
 		return err
 	}
-	tmp := buf.Bytes()[:buf.Len()-1]
 	switch msg.EncryptMethod {
 	case 0x00:
 	case 0x03:
 		msg.ShareKey = msg.RandomKey
 	}
-	buf = bytes.NewBuffer(crypto.NewCipher(msg.ShareKey).Decrypt(tmp))
+	buf = bytes.NewBuffer(crypto.NewCipher(msg.ShareKey).Decrypt(buf.Bytes()))
 	log.Printf("--> [recv] encryptMethod 0x%02x, dump oicq:\n%s", msg.EncryptMethod, hex.Dump(buf.Bytes()))
 	if err := unmarshalData(ctx, buf, msg); err != nil {
 		return err
@@ -31,17 +35,22 @@ func Unmarshal(ctx context.Context, data []byte, msg *Message) error {
 	return nil
 }
 
+func checkVaild(v []byte) (int, error) {
+	n := int(v[0])<<24 | int(v[1])<<16 | int(v[2])<<8 | int(v[3])<<0
+	if len(v) < n {
+		return 4, io.ErrUnexpectedEOF
+	}
+	if v[4] != 0x02 {
+		return 5, fmt.Errorf("unexpected prefix, got 0x%x", v[4])
+	}
+	if v[n-1] != 0x03 {
+		return n, fmt.Errorf("unexpected suffix, got 0x%x", v[n-1])
+	}
+	return n, nil
+}
+
 func unmarshalHead(ctx context.Context, buf *bytes.Buffer, msg *Message) error {
 	var err error
-	var tmp uint8
-	if _, err = buf.DecodeUint32(); err != nil {
-		return err
-	}
-	if tmp, err = buf.DecodeUint8(); err != nil {
-		return err
-	} else if tmp != 0x02 {
-		return fmt.Errorf("unexpected start, got 0x%x", tmp)
-	}
 	if _, err = buf.DecodeUint16(); err != nil {
 		return err
 	}
